455b1aa6203cd5a6c974037c5c93d98a1783df06
Fabricio C Zuardi <fabricio@fabricio.org>
2022-03-09 11:13:59 -0300

    ex 2 extra credit fix typo

--- ex2.rb
+++ ex2.rb
@@ -3,7 +3,7 @@
 
 puts "I could have code like this."
 
-# You can also use a comment to "disable" or comment out a puecce of code:
+# You can also use a comment to "disable" or comment out a piece of code:
 # puts "This won't run."
 
 puts "This will run."
