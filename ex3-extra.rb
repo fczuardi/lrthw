4a1b4d53b241d1e668adc39ae1d8b390986a0cc6
Fabricio C Zuardi <fabricio@fabricio.org>
2022-03-09 11:45:36 -0300

    ex3 extra

---
+++ ex3-extra.rb
@@ -0,0 +1,3 @@
+# Find something you need to calculate and write a new .rb file that does it
+
+puts "Seconds in a year", 365 * 24 * 60 * 60
--- ex3.rb
+++ ex3.rb
@@ -1,14 +1,23 @@
 puts "I will now count my chickens:"
 
+# 25 + (30/6)
 puts "Hens", 25 + 30 / 6
+# 100 - ((25*3) % 4)
+# % is the Modulus operator, remainder of the division
 puts "Roosters", 100 - 25 * 3 % 4
 
 puts "Now I will count the eggs:"
 
+# 3 + 2 + 1 - 5 + (0) - (1/4) + 6
+# integer division, so 1/4 is 0
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# same expression but using floating points
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4.0 + 6
 
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+# (3 + 2) < (5 - 7)
 puts 3 + 2 < 5 - 7
 
 puts "What is 3 + 2?", 3 + 2
